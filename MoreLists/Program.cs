6fb32bb543015e99b8450745216e1162390fd2e0
Odie de Moura <odie.demoura@shipsource.com>
2017-03-07 14:16:50 -0700

    More Lists assignment complete

---
+++ MoreLists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            var helloPasswords =
+                from user in users
+                where user.Password == "hello"
+                select user.Password;
+
+            foreach (string password in helloPasswords)
+            {
+                Console.WriteLine($"Password: {password}");
+            }
+
+            users.RemoveAll( user => user.Password == user.Name.ToLower());
+
+            var firstWithHello = users.Where( user => user.Password == "hello").First();
+
+            users.Remove(firstWithHello);
+
+            Console.WriteLine();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("******Users Left ******");
+                Console.WriteLine($"Username: {user.Name}, Password: {user.Password}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
