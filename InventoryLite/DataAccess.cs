d48498781808b0dc6a75d253e4bc5eb2ad694bd2
OdieD8 <odie_de_moura@hotmail.com>
2017-03-18 19:13:46 -0600

    Started class project

---
+++ InventoryLite/DataAccess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryLite
+{
+    class DataAccess
+    {
+        public static string DSNHost = @"SERVER=localhost\SQLEXPRESS;DATABASE=InventoryLite;Trusted_Connection=Yes;";
+
+        public DataAccess()
+        {
+
+        }
+
+        public static DataSet GetItems()
+        {
+            string query = "Select * FROM VAD ORDER BY Category ASC";
+
+            DataSet dataSet = SendSelectQuery(DSNHost, query);
+
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                return dataSet;
+            }
+            return null;
+        }
+
+        public static DataSet SendSelectQuery(string connectionString, string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connection);
+                sqlDataAdapter.Fill(dataSet);
+                connection.Close();
+                return dataSet;
+            }
+        }
+
+        public static int SendNonSelectQuery(string connectionString, string query)
+        {
+            int result = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    result = cmd.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+            return result;
+        }
+    }
+}
---
+++ InventoryLite/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace InventoryLite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataSet allItems = DataAccess.GetItems();
+
+            //List<DataRow> itemsList = allItems.Tables[0].AsEnumerable().ToList();
+
+            inventoryItems.ItemsSource = allItems.Tables[0].DefaultView;
+        }
+
+        public void OnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void OnOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void OnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void OnFind_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void OnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
